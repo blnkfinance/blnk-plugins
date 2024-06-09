a6538e6b8321f3ff23b400bcec72da17bcfb7848
jerry-enebeli <jerryenebeli@gmail.com>
2024-06-09 15:30:33 +0100

    update plugin manifest

--- plugins/hello-world/src/index.js
+++ plugins/hello-world/src/index.js
@@ -2,12 +2,16 @@ async function init() {
   console.log("Hello World Plugin initialized");
 }
 
-async function run(eventData) {
-  console.log("Hello World Plugin executed with event data:", eventData);
+async function run(blnk) {
+  console.log("Hello World Plugin executed with event data:", blnk);
 }
 
 async function terminate() {
   console.log("Hello World Plugin terminated");
+}
+
+async function subscribe(event) {
+  console.log("Something happened in the ledger: ", event);
 }
 
 module.exports = { init, run, terminate };
